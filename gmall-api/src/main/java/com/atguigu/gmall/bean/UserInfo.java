c3b24336a2c3a2c1e59bfbc88617b4c4b824fd81
飞龙 <769433609@qq.com>
2021-09-16 10:44:07 +0800

    两表联合查询 整合项目-api,common-util,service-util.. 版本控制 gmall-parent

---
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/UserInfo.java
@@ -0,0 +1,109 @@
+package com.atguigu.gmall.bean;
+
+import java.util.List;
+
+public class UserInfo {
+    private String id;
+    private String loginName;
+    private String nickName;
+    private String passwd;
+    private String name;
+    private String phoneNum;
+    private String email;
+    private String headImg;
+    private String userLevel;
+
+    public List<UserAddress> getUserAddressList() {
+        return userAddressList;
+    }
+
+    public void setUserAddressList(List<UserAddress> userAddressList) {
+        this.userAddressList = userAddressList;
+    }
+
+    private List<UserAddress> userAddressList;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(String userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public UserInfo(String id, String login_name, String nick_name, String passwd, String name, String phone_num, String email, String head_img, String user_level) {
+        this.id = id;
+        this.loginName = login_name;
+        this.nickName = nick_name;
+        this.passwd = passwd;
+        this.name = name;
+        this.phoneNum = phone_num;
+        this.email = email;
+        this.headImg = head_img;
+        this.userLevel = user_level;
+    }
+}
